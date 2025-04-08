a2e700ae410e11da4c5506d68ce345e9d04f3583
erfanhosseinabadi <erfnho3einabadi@gmail.com>
2025-04-08 17:51:02 +0300

    vector&matrix

--- vect&matrix.cpp
+++ vect&matrix.cpp
@@ -61,7 +61,120 @@ public:
     cout<<endl;
    }
 };
-
+class matrix {
+    public:
+        int rows, cols;
+        int** mat;
+    
+        matrix(int r, int c): rows(r), cols(c) {
+            mat = new int*[rows];
+            for(int i = 0; i < rows; ++i) {
+                mat[i] = new int[cols];
+                for(int j = 0; j < cols; ++j) {
+                    mat[i][j] = 0;
+                }
+            }
+            cout << "matrix created" << endl;
+        }
+    
+        ~matrix() {
+            for(int i = 0; i < rows; ++i) {
+                delete[] mat[i];
+            }
+            delete[] mat;
+            cout << "matrix destroyed" << endl;  // Fixed message
+        }
+    
+        matrix(const matrix& other) : rows(other.rows), cols(other.cols) {
+            mat = new int*[rows];
+            for(int i = 0; i < rows; ++i) {
+                mat[i] = new int[cols];
+                for(int j = 0; j < cols; ++j) {
+                    mat[i][j] = other.mat[i][j];
+                }
+            }
+        }
+    
+        matrix& operator=(const matrix& other) {
+            if(this == &other) {
+                return *this;
+            }
+    
+            for(int i = 0; i < rows; ++i) {
+                delete[] mat[i];
+            }
+            delete[] mat;
+    
+            rows = other.rows;
+            cols = other.cols;
+    
+            mat = new int*[rows];
+            for(int i = 0; i < rows; ++i) {
+                mat[i] = new int[cols];
+                for(int j = 0; j < cols; ++j) {
+                    mat[i][j] = other.mat[i][j];
+                }
+            }
+            return *this;
+        }
+    
+        matrix operator+(const matrix& other) const {
+            if(rows != other.rows || cols != other.cols) {
+                cout << "Matrices have different dimensions!" << endl;
+                return matrix(0, 0);
+            }
+            matrix result(rows, cols);
+            for(int i = 0; i < rows; ++i) {
+                for(int j = 0; j < cols; ++j) {
+                    result.mat[i][j] = mat[i][j] + other.mat[i][j];
+                }
+            }
+            return result;
+        }
+    
+        matrix operator*(const matrix& other) const {
+            if(rows != other.rows || cols != other.cols) {
+                cout << "Matrices have different dimensions!" << endl;
+                return matrix(0, 0);
+            }
+            matrix result(rows, cols);
+            for(int i = 0; i < rows; ++i) {
+                for(int j = 0; j < cols; ++j) {
+                    result.mat[i][j] = mat[i][j] * other.mat[i][j];
+                }
+            }
+            return result;
+        }
+    
+        matrix operator*(int k) const {
+            matrix result(rows, cols);
+            for(int i = 0; i < rows; ++i) {
+                for(int j = 0; j < cols; ++j) {
+                    result.mat[i][j] = k * mat[i][j];
+                }
+            }
+            return result;
+        }
+    
+        matrix operator-() const {
+            matrix result(rows, cols);
+            for(int i = 0; i < rows; ++i) {
+                for(int j = 0; j < cols; ++j) {
+                    result.mat[i][j] = -mat[i][j];
+                }
+            }
+            return result;
+        }
+    
+        void print() const {
+            for(int i = 0; i < rows; ++i) {
+                for(int j = 0; j < cols; ++j) {
+                    cout << mat[i][j] << ", ";
+                }
+                cout << endl;
+            }
+        }
+    };
 int main(){
     vector<int> v1(3);
     v1.v[0]=1,v1.v[1]=2,v1.v[2]=3;
@@ -83,6 +196,30 @@ int main(){
     vector<int> v6=v1*6;
     cout<<"v6: ";
     v6.print();
+
+    matrix m1(2,3);
+    m1.mat[0][0]=1,m1.mat[0][1]=2,m1.mat[0][2]=3;
+    m1.mat[1][0]=4,m1.mat[1][1]=5,m1.mat[1][2]=6;
+    matrix m2(2,3);
+    m2.mat[0][0]=7,m2.mat[0][1]=8,m2.mat[0][2]=9;
+    m2.mat[1][0]=10,m2.mat[1][1]=11,m2.mat[1][2]=12;
+    cout<<"m1: "<<endl;
+    m1.print();
+    cout<<"m2; "<<endl;
+    m2.print();
+    matrix m3=m1+m2;
+    cout<<"m3: "<<endl;
+    m3.print();
+    matrix m4=m1*m2;
+    cout<<"m4: "<<endl;
+    m4.print();
+    matrix m5=m1*2;
+    cout<<"m5: "<<endl;
+    m5.print();
+    matrix m6 = -m1;
+    cout<<"-m1: "<<endl;
+    m6.print();
+
     return 0;
 
 
